9b0b9556892f5e7ab6dab4ee5995a5f79c8939ca
Nurtas Ilyas <inurtas1999@gmail.com>
2018-05-28 23:57:52 +0600

    Update constants.py

--- constants.py
+++ constants.py
@@ -2,11 +2,11 @@ from emoji import emojize
 
 # constants
 
-token = "497558459:AAH5MwqMPP25GMADY5tPM_6Ej5PfT3dNHec"
-shortener_key = 'AIzaSyBSNqANV8p1tcQkECi6XKD98Ubb-McE9Ns'
-shortener_url = 'https://www.googleapis.com/urlshortener/v1/url?key=AIzaSyBSNqANV8p1tcQkECi6XKD98Ubb-McE9Ns'
+token = "Your Token"
+shortener_key = 'Your Key'
+shortener_url = 'https://www.googleapis.com/urlshortener/v1/url?key=' + shortener_key
 translate_url = 'https://translate.yandex.net/api/v1.5/tr.json/translate?'
-translate_key = 'trnsl.1.1.20180414T171052Z.35fa0c867cd681d3.88357a14f07b92e4601ac59f6ab10965a51af354'
+translate_key = 'Your Translate Key'
 
 error_message = "Sorry! But there is something wrong in your input data! (Use the same format as in example)"
 tryagain_message = "Input Error! Please Try again! You should use same format as in Examples"
